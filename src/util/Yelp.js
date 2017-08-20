bb317e9c61dcf1cfe2d1af9282d730304d25a19e
Kris Erdahl <Erdahl@Kriss-MBP.domain>
2017-08-20 11:47:08 -0500

    template strings

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -27,7 +27,7 @@ const Yelp = {
 					`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
 					{
 						headers: {
-							Authorization: 'Bearer ${accessToken}'
+							Authorization: `Bearer${accessToken}`
 						}
 					}
 				);
