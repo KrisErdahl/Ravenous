1f2dc30294f0eeee0b359e7e8cc3d127f95d46c3
Kris Erdahl <Erdahl@Kriss-MBP.domain>
2017-08-22 16:31:26 -0500

    update Bearer token

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -16,25 +16,6 @@ const Yelp = {
 			.then(response => response.json())
 			.then(jsonResponse => (accessToken = jsonResponse.access_token));
 	},
-	// getAccessToken() {
-	// 	if (accessToken) {
-	// 		return new Promise(resolve => {
-	// 			resolve(accessToken);
-	// 		});
-	// 		return fetch(
-	// 			`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
-	// 			{
-	// 				method: 'POST'
-	// 			}
-	// 		)
-	// 			.then(response => {
-	// 				return response.json();
-	// 			})
-	// 			.then(jsonResponse => {
-	// 				accessToken = jsonResponse.access_token;
-	// 			});
-	// 	}
-	// },
 	search(term, location, sortBy) {
 		return Yelp.getAccessToken()
 			.then(() => {
@@ -42,16 +23,16 @@ const Yelp = {
 					`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
 					{
 						headers: {
-							Authorization: `Bearer${accessToken}`
+							Authorization: `Bearer ${accessToken}`
 						}
 					}
 				);
 			})
 			.then(response => response.json())
 			.then(jsonResponse => {
-				if (jsonResponse.business) {
+				if (jsonResponse.businesses) {
 					return jsonResponse.businesses.map(business => ({
-						id: business.rating,
+						id: business.id,
 						imageSrc: business.image_url,
 						name: business.name,
 						address: business.location.address1,
